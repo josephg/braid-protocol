e8af692e4d3c178917deea4a0b59b49bd5009747
Duane Johnson <duane.johnson@gmail.com>
2021-02-02 14:45:11 -0700

    Add cors to examples/1

--- examples/1/server.js
+++ examples/1/server.js
@@ -1,40 +1,42 @@
 const polka = require('polka')
+const cors = require('cors')
 const fs = require('fs')
 const sirv = require('sirv')
 const makeStream = require('@josephg/braid-server')
 
 const assets = sirv(__dirname + '/web')
 
-const getDate = () => (new Date().toLocaleString() + '\n')
+const getDate = () => new Date().toLocaleString() + '\n'
 
 polka()
-.get('/', (req, res) => {
-  // Could use sirv or something but eh.
-  res.setHeader('content-type', 'text/html')
-  res.end(fs.readFileSync('test.html'))
-})
-.get('/time', (req, res) => {
-  let timer
+  .use(cors({ origin: true }))
+  .get('/', (req, res) => {
+    // Could use sirv or something but eh.
+    res.setHeader('content-type', 'text/html')
+    res.end(fs.readFileSync('test.html'))
+  })
+  .get('/time', (req, res) => {
+    let timer
 
-  if (req.headers.subscribe === "keep-alive") {
-    const stream = makeStream(res, {
-      initialValue: getDate(),
-      contentType: 'text/plain',
-      onclose() {
-        clearInterval(timer)
-      }
-    })
+    if (req.headers.subscribe === 'keep-alive') {
+      const stream = makeStream(res, {
+        initialValue: getDate(),
+        contentType: 'text/plain',
+        onclose() {
+          clearInterval(timer)
+        },
+      })
 
-    timer = setInterval(() => {
-      stream.append({data: getDate()})
-    }, 1000)
-  } else {
-    res.end(getDate())
-  }
-})
-.use(assets)
-.listen(2001, err => {
-  if (err) throw err
-  console.log('listening on http://localhost:2001/time')
-  console.log('Open http://localhost:2001/ in a browser for a simple demo')
-})
+      timer = setInterval(() => {
+        stream.append({ data: getDate() })
+      }, 1000)
+    } else {
+      res.end(getDate())
+    }
+  })
+  .use(assets)
+  .listen(2001, (err) => {
+    if (err) throw err
+    console.log('listening on http://localhost:2001/time')
+    console.log('Open http://localhost:2001/ in a browser for a simple demo')
+  })
