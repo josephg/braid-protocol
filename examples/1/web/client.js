c80d34de19e008c1ec58ba481d0d8a84030b0cee
Seph Gentle <me@josephg.com>
2021-03-18 14:22:39 +1100

    Fixed example 1 web demo

--- examples/1/web/client.js
+++ examples/1/web/client.js
@@ -4,8 +4,8 @@ const elem = document.getElementById('time')
 
 elem.innerText = 'loading'
 ;(async () => {
-  const { stream } = await subscribe('http://localhost:2001/time')
-  for await (const { value } of stream) {
+  const { updates } = await subscribe('http://localhost:2001/time')
+  for await (const { value } of updates) {
     elem.innerText = value
   }
   elem.innerText = 'disconnected'
